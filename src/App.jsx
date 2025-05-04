85b592664829d02da20b4a08544a1a3c5236e8c6
upparakash <akashuppar1998@gmail.com>
2025-05-04 18:39:37 +0530

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <Navbar />
       
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="democompany/" element={<Home />} />
         <Route path="/careers" element={<Careers />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/aboutus" element={<AboutUs />} />
